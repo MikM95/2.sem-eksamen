b4c164df70603a773a0961475fe2842635dc4a54
MikM95 <73488233+MikM95@users.noreply.github.com>
2021-05-29 13:38:04 +0200

    ændring i hvordan header og footer er lavet

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <?php include('functions.php') ?>
-
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Ikea secondhand sales</title>
-		<link rel="stylesheet" href="css/master.css">
-	</head>
-	<body>
-  <?php include('templates/navigation.php'); ?>
+<?php
+include 'templates/header.php';
+ ?>
 
 
 
-	</body>
-</html>
+
+
+ <?php
+ include 'templates/footer.php';
+  ?>
---
+++ templates/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Ikea secondhand sales</title>
+		<link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="../css/nav.css">
+	</head>
+	<body>
+
+
+<nav>
+    <div class="wrapper">
+      <a href="index.php"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Ikea_logo.svg/2560px-Ikea_logo.svg.png" alt="Ikea logo" style="width:150px;height:auto;"></a>
+      <ul class="navigation">
+        <li> <a href="index.php">forside</a> </li>
+        <li> <a href="">2</a> </li>
+        <li> <a href="signup.php">Sign up</a> </li>
+        <li> <a href="login.php">Log in</a> </li>
+      </ul>
+    </div>
+  </nav>
+
+<div class="wrapper">
