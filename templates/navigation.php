88b5f53fa0b00b351b75e5fac21ccff1097f97de
MikM95 <73488233+MikM95@users.noreply.github.com>
2021-05-29 13:19:00 +0200

    ændring af nav bar, klargørelse til login og signup

--- templates/navigation.php
+++ templates/navigation.php
@@ -10,5 +10,6 @@
   <ul class="navigation">
     <li> <a href="">1</a> </li>
     <li> <a href="">2</a> </li>
-    <li> <a href="">3</a> </li>
+    <li> <a href="signup.php">Sign up</a> </li>
+    <li> <a href="login.php">Log in</a> </li>
   </ul>
