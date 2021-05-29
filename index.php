104de4aa70557d6da0cf8ef07d693c243d5a9ea9
MikM95 <73488233+MikM95@users.noreply.github.com>
2021-05-29 12:54:14 +0200

    nav bar opstart med ikea logo

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include('functions.php') ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Ikea secondhand sales</title>
+		<link rel="stylesheet" href="css/master.css">
+	</head>
+	<body>
+  <?php include('templates/navigation.php'); ?>
+
+
+
+	</body>
+</html>
---
+++ templates/navigation.php
@@ -0,0 +1,14 @@
+<head>
+  <link rel="stylesheet" href="../css/nav.css">
+</head>
+
+  <a class="nav_logo" href="index.php">
+    <img id="ikea_logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Ikea_logo.svg/2560px-Ikea_logo.svg.png" alt="Ikea logo" style="width:150px;height:auto;">
+  </a>
+
+
+  <ul class="navigation">
+    <li> <a href="">1</a> </li>
+    <li> <a href="">2</a> </li>
+    <li> <a href="">3</a> </li>
+  </ul>
