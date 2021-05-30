3552f497ec70313789024277d56e73d4ebd2ff8f
Knudsen2 <73479499+Knudsen2@users.noreply.github.com>
2021-05-30 19:05:19 +0200

    Item-single - henter data fra databasen til item single siden

--- index.php
+++ index.php
@@ -8,22 +8,38 @@ include 'templates/header.php';
 aside
 </aside>
 <main class="flex-child-main">
-<?php $db_data = performQuery("SELECT id, name, image, auc_end FROM user_items");
+<?php $db_data = performQuery("SELECT id, title, image, auc_end, start_price FROM user_items");
 while($output_data = mysqli_fetch_assoc($db_data)) { ?>
   <a href="item-single.php?item_id=<?php echo $output_data['id'];?>">
     <div class="flex_child_of_child">
-      <p class="bold-big-text"><?php echo $output_data['name']; ?></p>
+      <p class="bold-big-text"><?php echo $output_data['title']; ?></p>
       <p>Slutter d.:<?php echo $output_data['auc_end']; ?> </p>
-        <?php
-        $item_id = $output_data['id'];
+      <?php $item_id = $output_data['id'];
+      $start_price = $output_data['start_price'];
+
         $db_bid_data = performQuery("SELECT bid_amount FROM bid WHERE item_id =$item_id ORDER BY bid_amount desc limit 1");
-        while($new = mysqli_fetch_assoc($db_bid_data)) { ?>
-        <p>Højeste bud: <?php echo $new['bid_amount']; ?> </p>
-      <?php } ?>
+
+        while($new = mysqli_fetch_assoc($db_bid_data)) {
+          $currentbid = $new['bid_amount'];
+
+
+        if ($currentbid < $start_price) {
+          echo $output_data['start_price'];
+        } else { ?>
+          <p>Højeste bud: <?php echo $new['bid_amount'];?> </p>
+        <?php }} ?>
+
+<?php //jeg har det problem at siden ikke vil vise startprisen på en vare hvis der ikke er kommet et bud på varen. På vare 2 har jeg lagt et bud ind i databasen som er under startprisen og så virker det. Det er dog ikke meningen at man skal kunne byde under startprisen. En mulig løsning er at lave det sådan at man opretter et bud på startprisen når man sætter en ting til salg, men det giver problemer hvis ingen byder på varen, så vinder man den selv? der må være en smartere måde at lave det på. spørg kenneth tirsdag ?>
+
+
+
+
+
+
 
 
     </div>
-    </a>
+  </a>
 <?php } ?>
 
 
--- item-single.php
+++ item-single.php
@@ -5,7 +5,32 @@ include 'templates/header.php';
 $item_id = $_GET['item_id'];
 ?>
 
-Du er nu på auktions siden for genstanden med id <?php echo $item_id; ?>
+<?php
+$db_data_item_single = performQuery("SELECT title, users.f_name, users.l_name, created_at, start_price, image, description, auc_end, bid.bid_amount from user_items inner join users on user_items.user_id = users.id inner join bid on bid.item_id = user_items.id where user_items.id=$item_id order by bid_amount desc limit 1");
+while ($row = mysqli_fetch_assoc($db_data_item_single)) { ?>
+  <p>Auktion for: <?php echo $row['title']; ?></p>
+  <p>Sælger: <?php echo $row['f_name'] . " " . $row['l_name']; ?></p>
+  <p>Auktionen startede d.: <?php echo $row['created_at']; ?></p>
+  <p>Nuværende højeste bud: <?php echo $row['bid_amount']; ?></p>
+  <?php
+    $end_of_auction = strtotime($row['auc_end']);
+    $sec_left = $end_of_auction - time();
+    $days_left = floor($sec_left / (60*60*24));
+    $hours_left_minus_days = floor(($sec_left - ($days_left * (60*60*24))) / (60*60));
+    $min_left_minus_days_and_hours = floor(($sec_left - $hours_left_minus_days * (60*60)-$days_left * (60*60*24)) / 60);
+    $sec_left_minus_everything = floor($sec_left - ($min_left_minus_days_and_hours * 60) - ($hours_left_minus_days * 60*60) - ($days_left * 60*60*24));
+  ?>
+  <p>Auktionen slutter om: <?php echo $days_left; ?> dage, <?php echo $hours_left_minus_days; ?> timer, <?php echo $min_left_minus_days_and_hours; ?> minutter og <?php echo $sec_left_minus_everything; ?> sekunder! </p>
+  <p>Beskrivelse af produktet: <?php echo $row['description']; ?></p>
+
+<?php } ?>
+
+<form method="post">
+  <input type="number" name="bid" placeholder="">
+  <button type="submit">Afgiv bud</button>
+</form>
+
+
 
 
  <?php
