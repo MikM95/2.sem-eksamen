69e779f0592e70b05a339710a48a850917b3825e
MikM95 <73488233+MikM95@users.noreply.github.com>
2021-05-31 16:44:27 +0200

    profile viser profil info, mangler auction/bid data

--- profile.php
+++ profile.php
@@ -1,1 +1,24 @@
-//tiltænkt til at være siden hvor man ser ens egne info samt ongoing bids/auctions
+<?php
+include('includes/functions.inc.php');
+include('includes/dbconnect.inc.php');
+include 'templates/header.php';
+?>
+
+
+<?php
+  $userid = $_SESSION["userid"];
+
+$db_data_profile = performQuery("SELECT username, email, f_name, l_name from users where id= $userid");
+
+while ($profile = mysqli_fetch_assoc($db_data_profile)) { ?>
+  <p>Username: <?php echo $profile['username']; ?></p>
+  <p>Name: <?php echo $profile['f_name'] . " " . $profile['l_name']; ?></p>
+  <p>Email: <?php echo $profile['email']; ?></p>
+
+
+<?php } ?>
+
+
+  <?php
+  include 'templates/footer.php';
+   ?>
