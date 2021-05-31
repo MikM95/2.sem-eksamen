8048a1a8dafeb7efc2ad7bcda88d605a15222928
Knudsen2 <73479499+Knudsen2@users.noreply.github.com>
2021-05-31 17:27:26 +0200

    Forsøg på visning af egne auktioner

--- profile.php
+++ profile.php
@@ -14,10 +14,28 @@ while ($profile = mysqli_fetch_assoc($db_data_profile)) { ?>
   <p>Username: <?php echo $profile['username']; ?></p>
   <p>Name: <?php echo $profile['f_name'] . " " . $profile['l_name']; ?></p>
   <p>Email: <?php echo $profile['email']; ?></p>
-
-
 <?php } ?>
 
+
+<?php
+$db_own_auctions = performQuery("SELECT id, title, created_at, auc_end from user_items where user_id = $userid");
+while ($own_auctions = mysqli_fetch_assoc($db_own_auctions)) {
+  print_r($db_own_auctions);
+  if(empty($db_own_auctions)) {
+    echo "You have not created any auctions!";
+  }else {
+  echo "Virker ikke";
+//
+  //   echo $own_auctions['title'];
+  //   echo $own_auctions['created_at'];
+  //   echo $own_auctions['auc_end'];
+//}
+
+}
+}
+
+
+ ?>
 
   <?php
   include 'templates/footer.php';
