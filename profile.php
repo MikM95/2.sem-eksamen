b379fa3545386cd6b33661d8845429e39b4a3589
Knudsen2 <73479499+Knudsen2@users.noreply.github.com>
2021-06-02 13:42:58 +0200

    Mere data på vundne auktioner

--- profile.php
+++ profile.php
@@ -40,17 +40,23 @@ if (mysqli_num_rows($db_own_auctions) > 0) {
 
 // Echo af auktioner du har vundet
 
-$db_won_auctions = performQuery("SELECT * FROM bid inner join user_items on user_items.id = bid.item_id where bid.user_id = $userid and bid.winning_bid= 1");
-if (mysqli_num_rows($db_won_auctions) > 0 ) {
-  while ($won_auctions = mysqli_fetch_assoc($db_won_auctions)) { ?>
-    <p> Du har vundet følgende auktion: </p>
-    <p>Titel: <?php echo $won_auctions['title']; ?></p>
-    <p>Auktionen vundet d.: <?php echo $won_auctions['auc_end'];  ?></p>
-    <p>købt for: <?php echo $won_auctions['bid_amount'];  ?></p>
-    <?php // I det sidste felt skal der vises hvem sælgeren er ?>
-    <p> Købt af: <?php echo $won_auctions[''];  ?></p>
+$db_won_auctions = performQuery("SELECT bid.bid_amount, title, created_at, start_price, image, auc_end, f_name, l_name, email FROM bid
+inner join user_items on user_items.id = bid.item_id
+INNER JOIN users on user_items.user_id = users.id
+where bid.user_id = $userid and bid.winning_bid= 1");
+if (mysqli_num_rows($db_won_auctions) > 0 ) { ?>
+    <p> Du har vundet følgende auktioner: </p>
+    <?php
+        while ($won_auctions = mysqli_fetch_assoc($db_won_auctions)) { ?>
+        <p>Titel: <?php echo $won_auctions['title']; ?></p>
+        <?php // echo af image ?>
+        <p>Auktionen vundet d.: <?php echo $won_auctions['auc_end'];  ?></p>
+        <p>Auktionens startpris:  <?php echo $won_auctions['start_price'];  ?></p>
+        <p>købt for: <?php echo $won_auctions['bid_amount'];  ?></p>
+        <p>Købt af: <?php echo $won_auctions['f_name'] . " " . $won_auctions['l_name'];  ?></p>
+        <p>Kontakt sælger på: <?php echo $won_auctions['email'];  ?></p>
 
-    <?php }
+        <?php }
 } else {
   // Hvis antallet af rows vi får tilbage fra databasen er mindre end nul har brugeren ikke vundet nogle auktioner
   ?>
