2bd857df0bd665b010d9b103a9387acf164dac63
Knudsen2 <73479499+Knudsen2@users.noreply.github.com>
2021-06-05 02:14:12 +0200

    css and links on profile page

--- profile.php
+++ profile.php
@@ -6,18 +6,29 @@ include 'templates/header.php';
 
 
 <?php
-  $userid = $_SESSION["userid"];
+  $userid = $_SESSION["userid"];?>
+
+<div class="flex-container-center">
+  <div class="flex-child-profile">
+    <p class="bold-text">Your user profile</p>
+    <?php
+    // echo af profil data
+    $db_data_profile = performQuery("SELECT username, email, f_name, l_name from users where id= $userid");
+
+    while ($profile = mysqli_fetch_assoc($db_data_profile)) { ?>
+      <p>Username: <?php echo $profile['username']; ?></p>
+      <p>Name: <?php echo $profile['f_name'] . " " . $profile['l_name']; ?></p>
+      <p>Email: <?php echo $profile['email']; ?></p>
+
+    <?php } ?>
+  </div>
+</div>
 
 
-// echo af profil data
-$db_data_profile = performQuery("SELECT username, email, f_name, l_name from users where id= $userid");
 
-while ($profile = mysqli_fetch_assoc($db_data_profile)) { ?>
-  <p>Username: <?php echo $profile['username']; ?></p>
-  <p>Name: <?php echo $profile['f_name'] . " " . $profile['l_name']; ?></p>
-  <p>Email: <?php echo $profile['email']; ?></p>
-  <hr>
-<?php } ?>
+<div class="flex-container-auctions">
+
+<div class="flex-child-profile">
 
 
 <?php
@@ -25,19 +36,29 @@ while ($profile = mysqli_fetch_assoc($db_data_profile)) { ?>
 // echo af egne auktioners data
 $db_own_auctions = performQuery("SELECT id, title, created_at, auc_end from user_items where user_id = $userid");
 if (mysqli_num_rows($db_own_auctions) > 0) {
-  ?> <p>You have created the following auctions: </p> <?php
-  while ($own_auctions = mysqli_fetch_assoc($db_own_auctions)) {?>
+  ?> <p class="bold-text">You have created the following auctions: </p> <?php
+  while ($own_auctions = mysqli_fetch_assoc($db_own_auctions)) {
+  $item_id = $own_auctions['id'];
+  ?><a href="item-single.php?item_id=<?php echo $item_id; ?>">
   <p>Title: <?php echo $own_auctions['title']; ?></p>
   <p>Auction created at: <?php echo $own_auctions['created_at']; ?>
-  <p>The auction ends: <?php echo $own_auctions['auc_end']; ?>
-<hr><?php
+  <p class="bottom-border">The auction ends: <?php echo $own_auctions['auc_end']; ?>
+  </a>
+<?php
   //der skal også printes data af hvem der evt har vundet auktionen, og hvad det vindende bud var på
 }
 } else {
-  echo "You have not created any auctions!";
-  ?><hr><?php
+  ?>
+  <p class="bold-text"><?php echo "You have not created any auctions!";  ?></p>
+  <?php
 }
+?>
+</div>
 
+<div class="flex-child-profile">
+
+
+<?php
 // echo af auktioner du har aktive bud på
 // Det er blevet til noget af en smøre - tænker vi tager den sammen senere
 // jeg har brugt group by item id fordi jeg kun ønsker at trække den enkelte auktion ud en gang - hvis man kommer i en bud kamp kan man nemt byde mange gange på et produkt - men vi er kun interesseret i at de auktioner man er i kommer ud en gang. Tænker evt at spørge kenneth og det er den bedste måde at gøre det på.
@@ -57,7 +78,7 @@ GROUP by bid.item_id");
   if (mysqli_num_rows($db_active_auction_bids) > 0) {
     // code...
     ?>
-    <p>You have active bids on the following auction(s): </p>
+    <p class="bold-text">You have active bids on the following auction(s): </p>
     <?php
         while($active_auction_bids = mysqli_fetch_assoc($db_active_auction_bids)){
           $item_id = $active_auction_bids['item_id']; ?>
@@ -79,48 +100,55 @@ GROUP by bid.item_id");
                 <?php } ?>
 
           <p>The seller is: <?php echo $active_auction_bids['f_name'] . " " . $active_auction_bids['l_name'];  ?></p>
-          <p>Mail the seller with further questions: <?php echo $active_auction_bids['email'];  ?></p>
+          <p class="bottom-border">Mail the seller with further questions: <?php echo $active_auction_bids['email'];  ?></p>
           </a>
-          <hr>
+
     <?php }
   }
   else {
     // code...
-    echo "You have no active bids";
-    ?><hr><?php
+    ?><p class="bold-text"><?php echo "You have no active bids"; ?></p>
+<?php
 
   }
+  ?>
+</div>
+<div class="flex-child-profile">
+<?php
 
 
 // Echo af auktioner du har vundet
 
-$db_won_auctions = performQuery("SELECT bid.bid_amount, title, created_at, start_price, image, auc_end, f_name, l_name, email FROM bid
+$db_won_auctions = performQuery("SELECT user_items.id, bid.bid_amount, title, created_at, start_price, image, auc_end, f_name, l_name, email FROM bid
 inner join user_items on user_items.id = bid.item_id
 INNER JOIN users on user_items.user_id = users.id
 where bid.user_id = $userid and bid.winning_bid= 1");
 if (mysqli_num_rows($db_won_auctions) > 0 ) { ?>
-    <p> You have won the following auction(s): </p>
+    <p class="bold-text"> You have won the following auction(s): </p>
     <?php
-        while ($won_auctions = mysqli_fetch_assoc($db_won_auctions)) { ?>
-        <p>Titel: <?php echo $won_auctions['title']; ?></p>
+        while ($won_auctions = mysqli_fetch_assoc($db_won_auctions)) {
+        $id_item = $won_auctions['id']; ?>
+        <a href="item-single.php?item_id=<?php echo $id_item; ?>">
+        <p>Title: <?php echo $won_auctions['title']; ?></p>
         <?php // echo af image ?>
         <p>The auction ended:  <?php echo $won_auctions['auc_end'];  ?></p>
         <p>Startprice:  <?php echo $won_auctions['start_price'];  ?></p>
         <p>You bought it for:  <?php echo $won_auctions['bid_amount'];  ?></p>
         <p>Seller:  <?php echo $won_auctions['f_name'] . " " . $won_auctions['l_name'];  ?></p>
-        <p>Contact seller with further questions:  <?php echo $won_auctions['email'];  ?></p>
-        <hr>
+        <p class="bottom-border">Contact seller with further questions:  <?php echo $won_auctions['email'];  ?></p>
+      </a>
         <?php }
 } else {
   // Hvis antallet af rows vi får tilbage fra databasen er mindre end nul har brugeren ikke vundet nogle auktioner
   ?>
-  <p><?php echo "You have not won any auctions yet"; ?> </p>
-  <hr>
+  <p class="bold-text"><?php echo "You have not won any auctions yet"; ?> </p>
+
   <?php }
 
 
  ?>
-
+ </div>
+</div>
   <?php
   include 'templates/footer.php';
    ?>
